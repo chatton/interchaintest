2302b26c828b5d76d9b6c5eda8b84c131b4434c4
chatton <github.qpeyb@simplelogin.fr>
2025-04-29 10:19:19 +0100

    test: adding E2ESimple

--- examples/example_test.go
+++ examples/example_test.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"github.com/cosmos/cosmos-sdk/types/module/testutil"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
 
 	celestiaapp "github.com/celestiaorg/celestia-app/v4/app"
+	"github.com/celestiaorg/celestia-app/v4/test/util/testnode"
 	"github.com/chatton/interchaintest/v1"
 	"github.com/chatton/interchaintest/v1/chain/cosmos"
 	"github.com/chatton/interchaintest/v1/ibc"
@@ -85,4 +87,41 @@ func TestCelestiaChain(t *testing.T) {
 	require.Equal(t, numValidators, len(cosmosChain.Validators))
 
 	t.Logf("Successfully started Celestia chain with %d validators", numValidators)
+
+	pollCtx, cancel := context.WithTimeout(ctx, 10*time.Minute)
+	defer cancel()
+
+	const requiredTxs = 10
+	const pollInterval = 5 * time.Second
+
+	// periodically check for transactions until timeout or required transactions are found
+	ticker := time.NewTicker(pollInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			// Check for transactions
+			blockchain, err := testnode.ReadBlockchainHeaders(ctx, celestia.GetHostRPCAddress())
+			if err != nil {
+				t.Logf("Error reading blockchain headers: %v", err)
+				continue
+			}
+
+			totalTxs := 0
+			for _, blockMeta := range blockchain {
+				totalTxs += blockMeta.NumTxs
+			}
+
+			t.Logf("Current transaction count: %d", totalTxs)
+
+			if totalTxs >= requiredTxs {
+				t.Logf("Found %d transactions, continuing with test", totalTxs)
+				return
+			}
+		case <-pollCtx.Done():
+			t.Logf("Timed out waiting for %d transactions", requiredTxs)
+			t.Failed()
+		}
+	}
 }
